bb0cea6daeecddc6ae327914397b987820763709
Gokul <gokulchandan18@gmail.com>
2022-01-03 14:53:30 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   pageTitle="gokul learns angular";
   imgUrl='https://upload.wikimedia.org/wikipedia/en/5/5d/Wolverine_%28James_%27Logan%27_Howlett%29.png';
 
+  imgUrl2='https://miro.medium.com/max/1155/1*C1kgFbqTyVuckvJ3Z1lgeQ.png';
+
 
   count=0;
   name!: string;
