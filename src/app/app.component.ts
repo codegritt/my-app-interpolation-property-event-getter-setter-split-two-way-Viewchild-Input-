0ee291ec4921d3b79c08486d9f84b2eaca181585
Gokul <gokulchandan18@gmail.com>
2022-01-03 09:32:50 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
 
 
   pageTitle="gokul learns angular";
+  imgUrl="https://static.wikia.nocookie.net/marveldatabase/images/1/16/James_Howlett_%28Earth-616%29_from_Wolverine_Vol_7_1_Silva_Variant_cover_001.jpg/revision/latest?cb=20191123041154";
+  
 }
